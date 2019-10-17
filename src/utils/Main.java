90090a42156edb069c5c0fa6cb1ceda1d8bbbe59
rbidilica <56684683+rbidilica@users.noreply.github.com>
2019-10-17 17:16:52 +0300

    Am implementat un switch

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,5 +1,37 @@
 package utils;
+import java.util.Scanner;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Subiecte: ");
+		
+		System.out.println("1._Romana");
+		System.out.println("2._Matematica");
+		System.out.println("3._Fizica");
+		System.out.println("4._Informatica");
+		System.out.println("Introdu nota :");
+		
+		Integer x = sc.nextInt();
+		System.out.println("Alege subiectul :");
+		Integer op = sc.nextInt();
+		
+		switch(op) {
+		case 1:
+			System.out.println("Nota la romana este :" + x);
+			break;
+		case 2:
+			System.out.println("Nota la matematica este :" + x);
+			break;
+		case 3:
+			System.out.println("Nota la fizica este :" + x);
+			break;
+		case 4:
+			System.out.println("Nota la informatica este :" + x);
+			break;
+		
+		}
+	}
 
 }
